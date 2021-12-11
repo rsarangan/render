cdf0dc311eef2e65acb2256f365bd6be47794f73
rsarangan <rsarang@gmail.com>
2021-12-10 20:58:13 -0500

    jkbk
    m,

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    print("sarang here")
-    return 'sarang here'
+# @app.route('/')
+# def hello_world():
+#     print("sarang here")
+#     return 'sarang here'
+
+import streamlit as st
+
+st.radio('Pick one', ['cats', 'dogs'])
